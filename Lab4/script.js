c93393f0ab8c841ecc8af5dbcfda466c552250b2
VuNguyen201004 <vu332210@gmail.com>
2024-01-23 20:35:59 +0700

    codetiep

---
+++ Lab4/script.js
@@ -0,0 +1,463 @@
+'use strict';
+
+// DATA
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+
+  movementsDates: [
+    '2019-01-28T09:15:04.904Z',
+    '2019-04-01T10:17:24.185Z',
+    '2019-05-27T17:01:17.194Z',
+    '2019-07-11T23:36:17.929Z',
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-03-08T14:11:59.604Z',
+    '2020-03-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+
+  movementsDates: [
+    '2019-01-25T14:18:46.235Z',
+    '2019-02-05T16:33:06.386Z',
+    '2019-03-10T14:43:26.374Z',
+    '2019-04-25T18:49:59.371Z',
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-02-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
+};
+
+const accounts = [account1, account2];
+
+//////////////////////////////////////////////////////////////////
+// APP
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+// LEC 16)
+// 1.
+const startLogOutTimer = function () {
+  // 3.
+  // There is always this 1s delay after the app loads and the start of the timer. And also between logins. So let's export the timer callback into its own function, and run it right away
+  const tick = function () {
+    let minutes = String(parseInt(time / 60, 10)).padStart(2, '0');
+    let seconds = String(parseInt(time % 60, 10)).padStart(2, '0');
+    // console.log(minutes, seconds);
+
+    // Displaying time in element and clock
+    labelTimer.textContent = `${minutes}:${seconds}`;
+
+    // Finish timer
+    if (time === 0) {
+      // We need to finish the timer, otherwise it will run forever
+      clearInterval(timer);
+
+      // We log out the user, which means to fade out the app
+      containerApp.style.opacity = 0;
+      labelWelcome.textContent = 'Log in to get started';
+    }
+
+    // Subtract 1 second from time for the next iteration
+    time--;
+  };
+
+  // Setting time to 5 minutes in seconds
+  let time = 10 * 60;
+  // let time = 10;
+
+  tick();
+  const timer = setInterval(tick, 1000);
+
+  // LATER
+  return timer;
+};
+
+const printWelcome = function (name) {
+  const now = new Date();
+  const greetings = new Map([
+    [[6, 7, 8, 9, 10], 'Chào buổi sáng'],
+    [[11, 12, 13], 'Chào buổi trưa'],
+    [[14, 15, 16, 17, 18], 'Chào buổi chiều'],
+    [[19, 20, 21, 22], 'Chào buổi tối'],
+    [[23, 0, 1, 2, 3, 4, 5], 'Good Night'],
+  ]);
+
+  const arr = [...greetings.keys()].find(key => key.includes(now.getHours()));
+  const greet = greetings.get(arr);
+  labelWelcome.textContent = `${greet}, ${name}!`;
+};
+
+// LEC 9)
+// 2.
+const formatMovementDate = function (date, locale) {
+  // LEC 12) add locale
+  const calcDaysPassed = (date1, date2) =>
+    Math.round((date1 - date2) / (60 * 60 * 24 * 1000));
+  const now = new Date();
+  const daysPassed = calcDaysPassed(now, date);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    return new Intl.DateTimeFormat(locale).format(date);
+  }
+};
+
+const formatCur = function (value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency,
+  }).format(value);
+};
+
+// LEC 8)
+// const printMovements = function(movements) {
+const printMovements = function (account, sort = false) {
+  containerMovements.innerHTML = '';
+
+  const mov = sort
+    ? account.movements.slice().sort((a, b) => a - b)
+    : account.movements;
+
+  mov.forEach((mov, i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    // LEC 8)
+    let printDate = '';
+    if (account.movementsDates) {
+      const date = new Date(account.movementsDates[i]);
+
+      // LEC 9)
+      printDate = formatMovementDate(date, account.locale);
+      // const day = `${date.getDate()}`.padStart(2, '0');
+      // // Remember that MONTHS are 0-based!
+      // const month = `${date.getMonth() + 1}`.padStart(2, '0');
+      // const year = date.getFullYear();
+      // printDate = `${day}/${month}/${year}`;
+    }
+
+    // LEC 14)
+    // Now we can finally use the user's locale and account currency!
+    // const formattedMov = new Intl.NumberFormat(account.locale, {
+    //   style: 'currency',
+    //   currency: account.currency,
+    //   // currency: 'USD',
+    // }).format(mov);
+    const formattedMov = formatCur(mov, account.locale, account.currency);
+
+    // LEC 4) + 14)
+    // <div class="movements__value">${mov.toFixed(2)}€</div>
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__date">${printDate}</div>
+        <div class="movements__value">${formattedMov}</div>
+      </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+// printMovements(account1.movements);
+printMovements(account1);
+
+const createUsernames = function (accounts) {
+  accounts.forEach(acc => {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+
+// LEC 14)
+// const calcPrintBalance = function(movements) {
+const calcPrintBalance = function (account) {
+  const balance = account.movements.reduce((accum, cur) => accum + cur, 0);
+  currentAccount.balance = balance;
+
+  // LEC 4)
+  // labelBalance.textContent = `${balance}€`;
+  // labelBalance.textContent = `${balance.toFixed(2)}€`;
+
+  // LEC 14)
+  labelBalance.textContent = formatCur(
+    balance,
+    account.locale,
+    account.currency,
+  );
+};
+
+const calcPrintSummary = function (account) {
+  const incomes = account.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, cur) => acc + cur, 0);
+  labelSumIn.textContent = formatCur(incomes, account.locale, account.currency);
+
+  const out = account.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, cur) => acc + cur, 0);
+  labelSumOut.textContent = formatCur(
+    Math.abs(out),
+    account.locale,
+    account.currency,
+  );
+
+  const interest = account.movements
+    .filter(mov => mov > 0)
+    .map(mov => mov * (currentAccount.interestRate / 100))
+    .filter(int => int > 1)
+    .reduce((acc, cur) => acc + cur, 0);
+  labelSumInterest.textContent = formatCur(
+    interest,
+    account.locale,
+    account.currency,
+  );
+};
+
+//////////////////////////////////////////////////////////////////
+let currentAccount, timer;
+// Lab 4.1
+btnLogin.addEventListener('click', function (e) {
+  // Ngăn sự kiện mặc định khi bấm nút submit sẽ không bị load trang
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value,
+  );
+
+  if (currentAccount && currentAccount.pin === +inputLoginPin.value) {
+    console.log(currentAccount);
+
+    // Xóa chữ ở thẻ input khi đăng nhập
+    inputLoginUsername.value = inputLoginPin.value = '';
+    // Làm mất focus ở thẻ input khi đăng nhập
+    inputLoginPin.blur();
+
+    // Làm hiện lên container từ opacity 0 -> 100
+    containerApp.style.opacity = 100;
+
+    // LEC 16)
+    // 2.
+    // If there is already a timer, then cancel it!
+    if (timer) clearInterval(timer);
+
+    // 1.
+    // Start 5 minutes timer to log out user automatically)
+    timer = startLogOutTimer();
+
+    // 2.
+
+    // Print welcome message
+    // LEC 10)
+    // labelWelcome.textContent = `Welcome back, ${
+    //   currentAccount.owner.split(' ')[0]
+    // }!`;
+    printWelcome(`${currentAccount.owner.split(' ')[0]}`);
+
+    // LEC 12)
+    // 1.
+    // Set current date and time!
+    const now = new Date();
+    const options = {
+      day: 'numeric',
+      month: 'numeric',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    };
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options,
+    ).format(now);
+
+    // Test with JD first, then with JS
+
+    // Print movements
+    // LEC 8)
+    printMovements(currentAccount);
+
+    // LEC 14)
+    // Print balance
+    calcPrintBalance(currentAccount);
+
+    // Print summary
+    calcPrintSummary(currentAccount);
+  }
+});
+//Lab 4.2 Chuyển tiền
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const receiver = accounts.find(acc => acc.username === inputTransferTo.value);
+  const amount = +inputTransferAmount.value;
+  // Kiểm tra người nhận tồn tại và khác username của mình và số tiền chuyển <= số tiền hiện có
+  if (
+    receiver &&
+    amount &&
+    currentAccount.balance >= amount &&
+    receiver.username !== currentAccount.username
+  ) {
+    currentAccount.movements.push(-amount);
+    receiver.movements.push(amount);
+
+    // LEC 8)
+    // 3.
+    currentAccount.movementsDates.push(new Date());
+    receiver.movementsDates.push(new Date());
+
+    // LEC 8)
+    printMovements(currentAccount);
+
+    // LEC 14)
+    calcPrintBalance(currentAccount);
+    calcPrintSummary(currentAccount);
+    clearInterval(timer);
+    timer = startLogOutTimer();
+  }
+
+  inputTransferTo.value = inputTransferAmount.value = '';
+});
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Math.floor(+inputLoanAmount.value);
+  // Nếu có bất kì số tiền gửi nào >= 10% số tiền vay thì cho vay
+  if (currentAccount.movements.some(mov => mov >= amount / 10) && amount > 0) {
+    currentAccount.movements.push(amount);
+
+    // LEC 8)
+    // 3.
+    currentAccount.movementsDates.push(new Date());
+
+    // LEC 15)
+    // 5.
+    setTimeout(() => {
+      // LEC 8)
+      printMovements(currentAccount);
+
+      // LEC 14)
+      calcPrintBalance(currentAccount);
+      calcPrintSummary(currentAccount);
+    }, 2500);
+
+    // LEC 16)
+    // 4.
+    clearInterval(timer);
+    timer = startLogOutTimer();
+  }
+  inputLoanAmount.value = '';
+});
+// Xóa accounnt
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  // Kiểm tra xem đúng tài khoản hiện tại chưa
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    +inputClosePin.value === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username,
+    );
+    // Sau khi kiểm tra xong sẽ thực hiện xóa tài khoản và làm mờ 
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+  // Đặt lại cho input rỗng
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function () {
+  printMovements(currentAccount, !sorted);
+  sorted = !sorted;
+});
+// Lab 4.3
+//1.
+const bankDepositSum = accounts
+.flatMap(acc => acc.movements)
+.filter(mov => mov > 0)
+.reduce((sum, cur) => sum + cur, 0);
+console.log(bankDepositSum);
+// 2.
+// const numDeposits1000 = accounts
+// .flatMap(acc => acc.movements)
+// .filter(mov => mov >= 1000).length;
+const numDeposits1000 = accounts
+.flatMap(acc => acc.movements)
+.reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+console.log(numDeposits1000);
+// Prefixed ++ oeprator
+let a = 10;
+console.log(++a);
+console.log(a);
+// 3.
+const { deposits, withdrawals } = accounts
+.flatMap(acc => acc.movements)
+.reduce(
+(sums, cur) => {
+// cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+return sums;
+},
+{ deposits: 0, withdrawals: 0 }
+);
+console.log(deposits, withdrawals);
+// 4.
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitzalize = str => str[0].toUpperCase() + str.slice(1);
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in',
+  'with'];
+  const titleCase = title
+  .toLowerCase()
+  .split(' ')
+  .map(word => (exceptions.includes(word) ? word : capitzalize(word)))
+  .join(' ');
+  return capitzalize(titleCase);
+  };
+  console.log(convertTitleCase('this is a nice title'));
+  console.log(convertTitleCase('this is a LONG title but not too long'));
+  console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+
